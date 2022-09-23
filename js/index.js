ba6bbd3919a2729f79b44cb5b30e75e421459f33
Gronnfrosk <Hanna.Fjeldsaa@stud.noroff.no>
2022-09-23 20:47:44 +0200

    Same as last

--- js/index.js
+++ js/index.js
@@ -276,7 +276,7 @@ function showSaved() {
 		Object.values(saveItems).map((item) => {
 			gameContainer.innerHTML += `
 		    <div class="gameSaved">
-                <a src="/html/furious.html><img src="/images/GameHub_covers${item.covers}_desktop.png"></a>
+                <a href="/html/furious.html"><img src="/images/GameHub_covers${item.covers}_desktop.png"></a>
             </div>
             `;
 		});
